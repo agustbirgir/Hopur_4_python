69c386200cd2ed3d668e8fa20763e5ffa61ce2c0
agustbirgir <54802234+agustbirgir@users.noreply.github.com>
2023-09-05 09:15:12 +0000

    Update fancy.py
    fixed

--- fancy.py
+++ fancy.py
@@ -1,7 +1,10 @@
 first = int(input())
 second = int(input())
 
-if:
+if first < 0 or second < 0 or first > 10**6 or second > 10**6:
+    print("Input values are out of the valid range.")
+
+else:
     result = 0
     for _ in range(second):
         result += first

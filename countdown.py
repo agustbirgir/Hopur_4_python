cad544b19e83c47beb37946e83740cf7c7eab98b
agustbirgir <54802234+agustbirgir@users.noreply.github.com>
2023-09-05 09:04:56 +0000

    setti inn dæmi 1 og 2

---
+++ countdown.py
@@ -0,0 +1,6 @@
+start = int(input()) # Do not change this line
+# Fill in the missing code below
+i = 0
+
+for i in range(start, 0, -1):
+        print(i)
---
+++ fancy.py
@@ -0,0 +1,11 @@
+first = int(input())
+second = int(input())
+
+if first < 0 or second < 0 or first > 10**6 or second > 10**6:
+    print("Input values are out of the valid range.")
+else:
+    result = 0
+    for _ in range(second):
+        result += first
+
+    print(result)

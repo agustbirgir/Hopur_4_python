e9ac92106af8dcbdf95a2dbc43b51cda1d2d751c
agustbirgir <54802234+agustbirgir@users.noreply.github.com>
2023-09-05 10:08:46 +0000

    þetta er retta ekki hitt

--- square.py
+++ square.py
@@ -1,13 +1,12 @@
 size = int(input())
 
-if 1 <= size <= 100:
-    print("* " * size) # prentar efsta partin i kassanum
-
-    for _ in range(size - 2):
-        print("* " + "  " * (size - 2) + "*")# prentar miðjuna i kassanum
-
-    
-    if size > 1:
-        print("* " * size)#prentar botnin í kassanum
-else:
-    print("Input value is out of the valid range.")
+for i in range(size):
+    for j in range(size):
+        if(i == 0 or i == size - 1 or j == 0 or j == size - 1):
+            if j != size - 1:
+                print("*", end=" ")
+            else:
+                print("*", end="")
+        else:
+            print(" ", end=" ")
+    print()
